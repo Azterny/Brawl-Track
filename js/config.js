5540ccc6c2334678fb188fdb00224330f6922a2a
Azterny <144600135+Azterny@users.noreply.github.com>
2025-12-24 22:39:34 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,9 @@
+// Configuration Globale
+const API_URL = "https://api.brawl-track.com"; 
 
+// Variables d'état partagées
+let currentUserTier = 'basic'; 
+let globalBrawlersList = [];
+let fullHistoryData = [];
+let currentLiveTrophies = null;
+let myChart = null; // Instance du graphique
