c53b0ed70a54467e3b258b65ea1aa6428c2f3bcb
Azterny <144600135+Azterny@users.noreply.github.com>
2026-03-04 01:36:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,4 +6,4 @@ let currentUserTier = 'basic';
 let globalBrawlersList = [];
 let fullHistoryData = [];
 let currentLiveTrophies = null;
-let myChart = null; // Instance du graphique
+window.myChart = null;
