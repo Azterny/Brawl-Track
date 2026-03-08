9bd61b2b4343a8348b4d27d938a50856b7d2f5c4
Azterny <144600135+Azterny@users.noreply.github.com>
2026-03-08 20:39:37 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,10 @@
+const GLOBAL_MAINTENANCE = true; // À synchroniser avec le 404.html
+const isBypassMode = new URLSearchParams(window.location.search).get('bypass') === 'true';
+
+if (GLOBAL_MAINTENANCE && !window.location.pathname.includes('maintenance.html') && !isBypassMode) {
+    window.location.replace('/maintenance.html');
+}
+
 // Configuration Globale
 const API_URL = "https://api.brawl-track.com"; 
 
