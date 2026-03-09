8ac2b565630497eccccaf649dacb28a726b3f76a
Azterny <144600135+Azterny@users.noreply.github.com>
2026-03-09 19:22:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const GLOBAL_MAINTENANCE = false;
+const GLOBAL_MAINTENANCE = true;
 const isBypassMode = new URLSearchParams(window.location.search).get('bypass') === 'true';
 
 if (GLOBAL_MAINTENANCE && !window.location.pathname.includes('maintenance.html') && !isBypassMode) {
